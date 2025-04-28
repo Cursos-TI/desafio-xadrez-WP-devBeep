8773073316b3a6097a0df7dd27a4a77ca206bbc1
WP-devBeep <wagnercp97@gmail.com>
2025-04-28 18:47:13 +0000

    criação dos loops

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    
+    // declaração das variáveis
+    int torre = 1, bispo = 1, rainha =1;
+    
+    // movimentação da torre usando a estrutura de loop while.
+    printf("\ntorre 5 casas para a direita\n");
+    while (torre <= 5)
+    {
+        printf("direita\n"),
+        torre++;
+    }
+    
+    // movimentação do bispo usando a estrutura de loop do-while.
+    printf("\nbispo 5 casas na diagonal superior direita\n");
+    do
+    {
+        printf("cima direita\n");
+        bispo++;
+    } while (bispo <= 5);
+    
+    // movimentação da rainha usando a estrutura de loop For.
+    printf("\nrainha 8 casas para a esquerda\n");
+    for ( rainha; rainha <= 8; rainha++)
+    {
+        printf("esquerda\n");
+    }
+    
+    
+    
+    
 
     return 0;
 }
